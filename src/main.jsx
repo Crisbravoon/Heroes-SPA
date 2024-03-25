1bdffdfb19c536febb58f2a1c2ebda3895e8d27c
Crisbravoon <cristobal_bravo6@hotmail.com>
2024-03-25 18:15:46 -0300

    Primera versión de proyecto

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import './styles.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Heroes } from './Heroes'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Heroes />
+  </React.StrictMode>,
+)
